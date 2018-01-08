8f23dff270ec5b24a0b72362075617b27f656ce6
jackobdiallo@gmail.com <jackobdiallo@gmail.com>
2018-01-08 17:01:45 +0100

    form person

--- www/scripts/form-person.js
+++ www/scripts/form-person.js
@@ -1,6 +1,6 @@
-// let person = new Person('Simplon','Jean', 23, true);
+let person = new Person('Simplon','Jean', 23, true);
 
-// console.log(person);
+ console.log(person);
 
 const formulaire = document.querySelector('#formulaire');
 
@@ -8,7 +8,17 @@ formulaire.addEventListener('submit', function(event){
     event.preventDefault();
 
     let name = document.querySelector('#name').value;
+    let surname = document.querySelector('#surname').value;
+    let age = document.querySelector('#age').value;
+    let isDev = document.querySelector('#isDev').checked;
+
 
     console.log(name);
+
+    let newperson = new Person(name, surname, age, isDev);
+    
+        console.log(newperson);
+
     
 });
+
